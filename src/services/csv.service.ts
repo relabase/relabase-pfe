be89195d1e454035f4f4c7f59ca27718e6ef36a6
Fabrice Lu <fabrice.lu.1@ens.etsmtl.ca>
2023-06-28 17:24:39 -0400

    csv start

---
+++ src/services/csv.service.ts
@@ -0,0 +1,31 @@
+import { parse } from 'papaparse';
+import { readFileSync } from 'fs';
+
+
+
+
+/**
+ * retourne les données du fichier csv
+ * 
+ * 
+ * @param filepath le répertoire du fichier (le répertoire 
+ * racine commence à partir du dossier src)
+ * 
+ */
+const csvParse = (filepath:string) =>
+{
+
+    var file = readFileSync(filepath, 'utf8');
+
+parse(file, {
+    complete: (result) => {
+        console.dir(result.data);
+        return result.data;
+    
+    
+    }
+});
+
+};
+
+
