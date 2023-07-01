adb3d338ae9ddc0ef8d3c296240c3bf203f1aba0
etsilhamenouri <ilhame.nouri.1@ens.etsmtl.ca>
2023-06-30 23:09:53 -0400

    generate rmd and output html file
    filename is currently the full date + time in YYYYMMDDHHMMSS format

--- src/controllers/analyze.controller.ts
+++ src/controllers/analyze.controller.ts
@@ -13,15 +13,16 @@ export class AnalyzeController {
 
   public sendScript = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      this.generateRFileFromString(String(req.body.script), next);
-      
-      const command = 'Rscript -e "source(\'src/input/input.R\')" > src/output/output.txt';
+      let filename: string = this.getTimestamp();
+      this.generateRFileFromString(String(req.body.script), filename, next);
+      let command: string = `Rscript -e "library(rmarkdown); rmarkdown::render(\'src/input/${filename}.Rmd\', output_format = \'html_document\', output_file = \'../output/${filename}.html\')"`;
       exec(command, (error, stdout, stderr) => {
         if (error) {
           console.error(`Error executing R script: ${error}`);
           return;
         }
-        console.log(stdout);
+        //if - parse output file to check for errors/if the file is empty
+        // console.log(stdout);
         res.status(200).json({ data: stdout, message: 'sent' });
       });
     } catch (error) {
@@ -29,12 +30,27 @@ export class AnalyzeController {
     }
   };
 
-  private generateRFileFromString = async (script: string, next: NextFunction): Promise<void> => {
+  private generateRFileFromString = async (script: string, filename: string, next: NextFunction): Promise<void> => {
     try {
-      fs.writeFileSync('src/input/input.R', script);
+      fs.writeFileSync(`src/input/${filename}.Rmd`, script);
     } catch (error) {
       next(error);
     }
   };
+
+  private getTimestamp(): string {
+    const now = new Date();
+
+    const year = now.getFullYear().toString().padStart(4, '0');
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    const timestamp = year + month + day + hours + minutes + seconds;
+
+    return timestamp;
+  }
 }
 
