78b5c64de6c965bab47596d5e3484ee6ec838877
Fabrice Lu <fabrice.lu.1@ens.etsmtl.ca>
2023-07-01 13:41:12 -0400

    fix connexion issue between container

--- src/database/MysqlConnect.ts
+++ src/database/MysqlConnect.ts
@@ -1,8 +1,7 @@
 import{ createConnection } from 'mysql2'
 
-//TODO test connection with docker 
 export const connection = createConnection({
-    host: "localhost",
+    host: "db-server",
     port: 3306,
     user: "root",
     password: "root",
