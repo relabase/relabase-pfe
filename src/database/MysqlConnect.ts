da46b87a8fbf495444a565574e4b4e1a9c090f63
Fabrice Lu <fabrice.lu.1@ens.etsmtl.ca>
2023-07-02 19:56:38 -0400

    switch to localhost

--- src/database/MysqlConnect.ts
+++ src/database/MysqlConnect.ts
@@ -1,7 +1,7 @@
 import{ createConnection } from 'mysql2'
 
 export const connection = createConnection({
-    host: "db-server",
+    host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
