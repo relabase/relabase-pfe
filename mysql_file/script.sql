78dd4ca5bba79749326b21f29b4077ce7a592ef9
Fabrice Lu <fabrice.lu.1@ens.etsmtl.ca>
2023-07-02 21:26:44 -0400

    forgot to update mysql file

--- mysql_file/script.sql
+++ mysql_file/script.sql
@@ -1,11 +1,13 @@
 use db;
+INSERT INTO role(nom_role) VALUE('admin');
+INSERT INTO role(nom_role) VALUE('user');
 
-insert user(email,password) value('example1@email.com','$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u');
-insert user(email,password) value('example2@email.com','$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u');
-insert user(email,password) value('example3@email.com','$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u');
-insert user(email,password) value('example4@email.com','$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u');
+insert user(email,password,id_role) value('example1@email.com','$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u',1);
+insert user(email,password,id_role) value('example2@email.com','$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u',1);
+insert user(email,password,id_role) value('example3@email.com','$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u',1);
+insert user(email,password,id_role) value('example4@email.com','$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u',1);
 
-INSERT INTO user(email,password) VALUE('test@test.com','sdasaasda');
+INSERT INTO user(email,password,id_role) VALUE('test@test.com','sdasaasda',1);
 
 
 
@@ -15,9 +17,7 @@ insert log(text) value('a text');
 
 INSERT INTO log(text) VALUE('aaaa'); 
 
-INSERT INTO role(nom_role) VALUE('testingrole');
-
-UPDATE user SET password = 'dwqwddqwd' WHERE id_user = 6;
+-- UPDATE user SET password = 'dwqwddqwd' WHERE id_user = 4;
 
 
 
