77393b993b8da123a9f84556d8597d0f3c8a0115
Fabrice Lu <fabrice.lu.1@ens.etsmtl.ca>
2023-07-04 02:37:57 -0400

    fix interface package req

--- src/interfaces/package_requests.interface.ts
+++ src/interfaces/package_requests.interface.ts
@@ -2,10 +2,8 @@ import { RowDataPacket } from "mysql2";
 
 export interface Package_request extends RowDataPacket{
   id_package_request?: number;
-  email:string;
-  access_reason: string;
-  is_approve: number;
-  first_name:string;
-  last_name:string;
-  filepath:string;
+  name_package:string;
+  reason:string;
+  is_approve:boolean;
+  id_user:number;
 }
