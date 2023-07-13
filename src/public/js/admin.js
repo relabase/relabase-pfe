ba7a236fd1a648e3b6650f0107320e9f65e90baf
kiaein <39696732+kiaein@users.noreply.github.com>
2023-07-13 02:51:05 -0400

    user application content init

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -65,8 +65,8 @@
     user.appendChild(arrow);
 
     user.dataset.name = 'Test ' + id;
-    user.dataset.email = 'Email ' + id;
-    user.dataset.applicationMessage = 'Application message ' + id;
+    user.dataset.email = 'email' + id + "@example.com";
+    user.dataset.applicationMessage = id + ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
     user.addEventListener('click', () => {
         const allUsers = document.querySelectorAll('.user-application');
