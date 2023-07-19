31732af28ffb34307bacae35ac0e545cdb886d83
kiaein <39696732+kiaein@users.noreply.github.com>
2023-07-19 06:09:40 -0400

    apply admin-utilities.js to user-application.js

--- src/public/js/admin/user-application.js
+++ src/public/js/admin/user-application.js
@@ -1,21 +1,20 @@
+import { hideContent, remove, checkEmptyList } from './admin-utilities.js';
+
 (function () {
   const UI = {
-    userApplications: document.querySelector('#pane1 .user-list'),
-    userContent: document.querySelector('.user-content'),
-    noUserSelectedMessage: document.querySelector('#no-content-selected'),
-    contentTitle: document.querySelector('#pane1 .large-header'),
-    emailLink: document.querySelector('#email-link'),
-    applicationMessage: document.querySelector('#application-message'),
-    approveButton: document.querySelector('#approve-button'),
-    declineButton: document.querySelector('#decline-button'),
+    // References to the elements in user-application.ejs
+    userApplications: document.querySelector('#pane1 .item-list.user-list'),
+    userContent: document.querySelector('#user-content'),
+    noUserSelectedMessage: document.querySelector('#user-no-content-selected'),
+    contentTitle: document.querySelector('#user-content .large-header'),
+    emailLink: document.querySelector('#user-content .email-link'),
+    applicationMessage: document.querySelector('#user-application-message'),
+    approveButton: document.querySelector('#user-content .approve-button'),
+    declineButton: document.querySelector('#user-content .decline-button'),
     idImage: document.querySelector('#user-id-image'),
   };
 
-  document.addEventListener('DOMContentLoaded', () => {
-    UI.userContent.classList.add('hide-content');
-    addUsers(generateDummyUsers(2));
-  });
-
+  // Generate dummy user data for testing
   function generateDummyUsers(count) {
     const users = [];
     for (let i = 1; i <= count; i++) {
@@ -30,6 +29,8 @@
     return users;
   }
 
+  // TODO: best way to do this?
+  // Create HTML string for a single user-application
   function createUserHtml(user) {
     return `
       <div class="user-application" id="${user.id}" data-name="${user.name}" data-email="${user.email}" data-application-message="${user.applicationMessage}">
@@ -42,22 +43,49 @@
     `;
   }
 
+  // Add users to the user applications list
   function addUsers(users) {
     const userHtml = users.map(createUserHtml).join('');
     UI.userApplications.innerHTML += userHtml;
+    checkEmptyList(UI.userApplications, UI.noUserSelectedMessage, 'No user to select', 'No user selected');
   }
 
-  UI.userApplications.addEventListener('click', function (e) {
-    let target = e.target;
-    while (target != this) {
-      if (target.classList.contains('user-application')) {
-        showUserContent(target.id, target.dataset.name, target.dataset.email, target.dataset.applicationMessage);
-        return;
-      }
-      target = target.parentNode;
-    }
-  });
+  // Approve user application
+  function approveUser(userId) {
+    //TODO: approve user logic
+    console.log("approved " + userId);
+    removeUser(userId);
+  }
 
+  // Decline user application
+  function declineUser(userId) {
+    //TODO: decline user logic
+    console.log("decline " + userId);
+    removeUser(userId);
+  }
+
+  // Remove user application from list
+  function removeUser(userId) {
+    remove(
+      userId,
+      UI.userApplications,
+      clearUserContent,
+      UI.noUserSelectedMessage,
+      'No user to select',
+      'No user selected'
+    );
+  }
+
+  // Clear out user content 
+  function clearUserContent() {
+    hideContent(UI.userContent, UI.noUserSelectedMessage);
+    UI.emailLink.href = '';
+    UI.emailLink.textContent = '';
+    UI.applicationMessage.textContent = '';
+    UI.idImage.src = '';
+  }
+
+  // Show the content of the application
   function showUserContent(userId, userName, userEmail, userApplicationMessage) {
     UI.userContent.classList.remove('hide-content');
     UI.noUserSelectedMessage.classList.add('hide-content');
@@ -74,33 +102,25 @@
     document.querySelector('.unblur-button').classList.remove('hidden');
   }
 
-  function hideUserContent() {
-    UI.userContent.classList.add('hide-content');
-    UI.noUserSelectedMessage.classList.remove('hide-content');
-  }
-
-  function approveUser(userId) {
-    console.log('approved ' + userId);
-    removeUser(userId);
-  }
-
-  function declineUser(userId) {
-    console.log('declined ' + userId);
-    removeUser(userId);
-  }
-
-  function removeUser(userId) {
-    let userElement = document.getElementById(userId);
-    let nextUser = userElement.nextElementSibling || userElement.previousElementSibling;
-    userElement.remove();
-
-    if (nextUser) {
-      nextUser.click();
-    } else {
-      hideUserContent();
+  // Event listener for clicking on user applications
+  UI.userApplications.addEventListener('click', function (e) {
+    let target = e.target;
+    while (target != this) {
+      if (target.classList.contains('user-application')) {
+        showUserContent(target.id, target.dataset.name, target.dataset.email, target.dataset.applicationMessage);
+        return;
+      }
+      target = target.parentNode;
     }
-  }
+  });
 
+  // Initial setup
+  document.addEventListener('DOMContentLoaded', () => {
+    UI.userContent.classList.add('hide-content');
+    addUsers(generateDummyUsers(2));
+  });
+
+  // Event listener for unblurring user ID image
   document.querySelector('.unblur-button').addEventListener('click', function () {
     this.classList.add('hidden');
     UI.idImage.style.filter = 'none';
