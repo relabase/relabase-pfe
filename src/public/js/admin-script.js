724f384de68b3bfd3746630c48067e6f09ef1032
kiaein <39696732+kiaein@users.noreply.github.com>
2023-07-19 06:07:42 -0400

    add logic to control visibility of tab contents

--- src/public/js/admin-script.js
+++ src/public/js/admin-script.js
@@ -1,27 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const tabs = document.querySelectorAll('.tab');
-    const tabContents = document.querySelectorAll('.tab-content > div');
-  
-    function deactivateTabs() {
-      tabs.forEach(tab => {
-        tab.classList.remove('active');
-      });
-  
-      tabContents.forEach(content => {
-        content.style.display = 'none';
-      });
-    }
-  
-    // Add click event listeners to the tabs
-    tabs.forEach((tab, index) => {
-      tab.addEventListener('click', () => {
-        deactivateTabs();
-  
-        tab.classList.add('active');
-        tabContents[index].style.display = 'block';
-      });
+  // Selecting all elements with class 'tab' and 'tab-content > div'
+  const tabs = document.querySelectorAll('.tab');
+  const tabContents = document.querySelectorAll('.tab-content > div');
+
+  // Function to deactivate all tabs
+  function deactivateTabs() {
+    tabs.forEach(tab => {
+      tab.classList.remove('active');
     });
-  
-    // Activate the first tab by default
-    tabs[0].click();
+    tabContents.forEach(content => {
+      content.classList.remove('active');
+    });
+  }
+
+  // Control visibility of tab UI
+  function manageTabSpecificUI(tabType) {
+    const noContentSelectedMessage = document.querySelector(`#${tabType}-no-content-selected`);
+    const itemContent = document.querySelector(`#${tabType}-pane .item-content`);
+
+    if (noContentSelectedMessage && itemContent) {
+      noContentSelectedMessage.classList.remove('hide-content');
+      itemContent.classList.add('hide-content');
+    }
+  }
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      deactivateTabs();
+      tab.classList.add('active');
+      tabContents[index].classList.add('active');
+      manageTabSpecificUI(tab.dataset.tab);
+    });
   });
+
+  // Activate first tab by default
+  tabs[0].click();
+});
