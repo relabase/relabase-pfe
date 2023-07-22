d49974dd89ec1338b1482f6bce5d4abd81d0b817
kiaein <39696732+kiaein@users.noreply.github.com>
2023-07-21 22:36:04 -0400

    add models with fake data for the admin page

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -2,8 +2,44 @@ import { User } from '@interfaces/users.interface';
 
 // password: password123456789
 export const UserModel: User[] = [
-  { id: 1, email: 'example1@email.com', password: '$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u' },
-  { id: 2, email: 'example2@email.com', password: '$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u' },
-  { id: 3, email: 'example3@email.com', password: '$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u' },
-  { id: 4, email: 'example4@email.com', password: '$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u' },
+  {
+    id: 1,
+    email: 'example1@email.com',
+    password: '$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u',
+    first_name: 'test',
+    last_name: '1',
+    id_role: 0,
+    image: '../img/test-id.jpg',
+    last_login: new Date(1632848787000),
+  },
+  {
+    id: 2,
+    email: 'example2@email.com',
+    password: '$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u',
+    first_name: 'test',
+    last_name: '2',
+    id_role: 1,
+    image: '../img/test-id.jpg',
+    last_login: new Date(1632848787000),
+  },
+  {
+    id: 3,
+    email: 'example3@email.com',
+    password: '$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u',
+    first_name: 'test',
+    last_name: '3',
+    id_role: 1,
+    image: '../img/test-id.jpg',
+    last_login: new Date(1632848787000),
+  },
+  {
+    id: 4,
+    email: 'example4@email.com',
+    password: '$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u',
+    first_name: 'test',
+    last_name: '4',
+    id_role: 1,
+    image: '../img/test-id.jpg',
+    last_login: new Date(1632848787000),
+  },
 ];
