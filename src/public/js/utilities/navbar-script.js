bc5ad5d8fc27f14fd88a0da9821c0ebca460b226
etsilhamenouri <ilhame.nouri.1@ens.etsmtl.ca>
2023-07-26 17:37:43 -0400

    fix log out button

--- src/public/js/utilities/navbar-script.js
+++ src/public/js/utilities/navbar-script.js
@@ -1,15 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     var dropdownMenu = document.querySelector('.navbar-item-group-right');
 
-    dropdownMenu.addEventListener('click', function () {
-        var dropdownContent = document.getElementById('dropdown-content');
-        if (dropdownContent.style.display === "none") {
-            dropdownContent.style.display = "block";
-        } else {
-            dropdownContent.style.display = "none";
-        }
-    });
-
     document.getElementById('log-out-button').onclick = () => {
         logout();
     }
