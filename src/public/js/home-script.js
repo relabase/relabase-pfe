a37fae38b090cf000d1b104259094246363fc017
kiaein <39696732+kiaein@users.noreply.github.com>
2023-07-26 19:56:21 -0400

    guest page init

--- src/public/js/home-script.js
+++ src/public/js/home-script.js
@@ -1,10 +1,14 @@
 window.onload = function () {
-  dateSearchFunction("home-date-from-search", "home-date-to-search", "home-table", 0);
-  enforceDateFormat(document.getElementById('home-date-from-search'));
-  enforceDateFormat(document.getElementById('home-date-to-search'));
+  var table = document.getElementById("home-table");
 
-  var tableRows = document.querySelectorAll(`#home-table .table-row`);
-  addClickListener(tableRows,
-    row => 'R Script generated on ' + row.children[0].textContent.trim()
-  );
+  if (table) {
+    dateSearchFunction("home-date-from-search", "home-date-to-search", "home-table", 0);
+    enforceDateFormat(document.getElementById('home-date-from-search'));
+    enforceDateFormat(document.getElementById('home-date-to-search'));
+
+    var tableRows = document.querySelectorAll(`#home-table .table-row`);
+    addClickListener(tableRows,
+      row => 'R Script generated on ' + row.children[0].textContent.trim()
+    );
+  }
 }
