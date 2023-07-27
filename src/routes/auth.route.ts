e39f0567936208f57bf0bac54fbcb30f988a0d89
etsilhamenouri <ilhame.nouri.1@ens.etsmtl.ca>
2023-07-26 21:41:05 -0400

    user multer to parse the file before sending it to the endpoint

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthController } from '@controllers/auth.controller';
 import { Routes } from '@interfaces/routes.interface';
 import { AuthMiddleware } from '@middlewares/auth.middleware';
+import multer from 'multer';
 
 export class AuthRoute implements Routes {
   public router = Router();
@@ -12,11 +13,25 @@ export class AuthRoute implements Routes {
   }
 
   private initializeRoutes() {
-    // this.router.post('/signup', ValidationMiddleware(CreateUserDto), this.auth.signUp);
+    const upload = this.initializeMulter();
     this.router.get('/logout', this.auth.logOut);
     this.router.get('/login', AuthMiddleware('login'), this.auth.getLoginPage);
     this.router.post('/authenticate', this.auth.redirect);
     this.router.get('/authenticate/client-id', this.auth.getClientId);
     this.router.get('/register', AuthMiddleware('register'), this.auth.getRegisterPage)
+    this.router.post('/register', [AuthMiddleware('register'), upload.single('file')], this.auth.submitUserApplication)
+  }
+
+  private initializeMulter = (): multer.Multer => {
+    const storage = multer.diskStorage({
+      destination: (req, file, cb) => {
+        cb(null, 'src/files/user_applications/');
+      },
+      filename: (req, file, cb) => {
+        const ext = file.mimetype.split("/")[1];
+        cb(null, `user_application_id_${Date.now()}.${ext}`);
+      },
+    });
+    return multer({ storage });
   }
 }
