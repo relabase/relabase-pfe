d7d8fca9f22377f7a589ce44f7a9d120c9ef5b60
etsilhamenouri <ilhame.nouri.1@ens.etsmtl.ca>
2023-07-26 20:37:47 -0400

    add error msg and validation for register fields

--- src/public/js/register-script.js
+++ src/public/js/register-script.js
@@ -23,9 +23,25 @@ window.onload = function () {
 
     //files
     document.getElementById('file-upload').onchange = () => {
-        console.log(document.getElementById('file-upload').files[0]);
         show(document.getElementById('file-name'));
         document.getElementById('file-name').innerHTML = document.getElementById('file-upload').files[0].name;
+    }
+
+    document.getElementById('submit-button').onclick = () => {
+        let first_name = document.getElementById('register-input-first-name').value;
+        let last_name = document.getElementById('register-input-last-name').value;
+        let email = document.getElementById('register-input-email').value;
+        let file = document.getElementById('file-upload').files[0];
+        let reason = document.getElementById('register-input-reason').value;
+        if (valid_string(first_name)
+            && valid_string(last_name)
+            && valid_email(email)
+            && valid_file(file)
+            && valid_string(reason)) {
+                hide(document.getElementById('register-error-msg'));
+        } else {
+            show(document.getElementById('register-error-msg'));
+        }
     }
 }
 
@@ -39,4 +55,25 @@ function show(div) {
     if (div.classList.contains('display-none')) {
         div.classList.remove('display-none');
     }
+}
+
+function valid_email(email) {
+    if (/^\S+@\S+\.\S+$/.test(email)) {
+        return true;
+    }
+    return false;
+}
+
+function valid_string(str) {
+    if (str === null || str === undefined || str.trim() === '') {
+        return false;
+    }
+    return true;
+}
+
+function valid_file(file) {
+    if (file === undefined) {
+        return false;
+    }
+    return true;
 }
